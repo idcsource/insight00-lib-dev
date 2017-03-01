2ae10119c10235b443ccf1e46a7b73aec80f4fdb
Fire Meditation <firemeditation@gmail.com>
2017-03-01 16:04:23 +0800

    add package porter

--- porter/struct.go
+++ porter/struct.go
@@ -24,13 +24,22 @@ import (
 	"github.com/idcsource/Insight-0-0-lib/cpool"
 	"github.com/idcsource/Insight-0-0-lib/ilogs"
 	"github.com/idcsource/Insight-0-0-lib/nst"
+	"github.com/idcsource/Insight-0-0-lib/roles"
 	"github.com/idcsource/Insight-0-0-lib/rolesio"
+)
+
+// 接收者接收后的处理方法，保存还是交由处理函数处理
+const (
+	OPERATE_TO_STORE = iota
+	OPERATE_TO_FUNCTION
 )
 
 // 货物，这是搬运工需要搬运的东西，也就是编码后的需要传递的角色信息，这里的编码使用hardstore所提供的方法
 type Cargo struct {
 	// 角色Id
 	Id string
+	// 发送者名字
+	SenderName string
 	// 角色的身体
 	RoleBody []byte
 	// 角色的关系
@@ -47,8 +56,12 @@ type Receiver struct {
 	listen *nst.TcpServer
 	// 身份验证码
 	code string
+	// 处理方法，OPERATE_TO_*
+	optype uint8
 	// 存储器实例
 	store rolesio.RolesInOutManager
+	// 注册的处理者
+	function ReceiverOperater
 	// 日志
 	logs *ilogs.Logs
 }
@@ -57,6 +70,8 @@ type Receiver struct {
 type Sender struct {
 	// 配置信息
 	config *cpool.Block
+	// 发送者名字
+	name string
 	// 接收者
 	receivers map[string]oneReceiver
 	// 日志
@@ -71,4 +86,9 @@ type oneReceiver struct {
 	code string
 	// 连接
 	tcpconn *nst.TcpClient
+}
+
+// 接收者的注册处理方法的接口
+type ReceiverOperater interface {
+	Operate(sendername string, role roles.Roleer) (err error)
 }
