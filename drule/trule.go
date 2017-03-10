fb4b54003131f6c39467eb25199a03ed786fce5e
Fire Meditation <firemeditation@gmail.com>
2017-03-10 14:36:29 +0800

    DRule

--- drule/trule.go
+++ drule/trule.go
@@ -116,7 +116,62 @@ func (t *TRule) handleCommitSignal(signal *tranCommitSignal) {
 
 // 处理rollback信号量
 func (t *TRule) handleRollbackSignal(signal *tranCommitSignal) {
+	// 给事务加锁
+	t.tran_lock.Lock()
+	defer t.tran_lock.Unlock()
 
+	// 构造返回
+	returnHan := tranReturnHandle{}
+	//找到这个事务
+	tran, find := t.transaction[signal.tran_id]
+	if find == false {
+		// 如果找不到
+		returnHan.Status = TRAN_RETURN_HANDLE_ERROR
+		returnHan.Error = fmt.Errorf("Can't find the transaction %v.", signal.tran_id)
+		signal.return_handle <- returnHan
+		return
+	}
+	// 开始大量的执行
+	// 给这个事务本身加锁
+	tran.lock.Lock()
+	defer tran.lock.Unlock()
+	// 遍历这里面所有的缓存角色
+	for roleid, rolec := range tran.tran_cache {
+		// 给这个角色加锁
+		rolec.lock.Lock()
+
+		// 检查等待队列
+		wait_count := len(rolec.wait_line)
+		if wait_count == 0 {
+			// 如果没有等待队列，将这个角色从缓存中移除
+			tran.tran_cache[roleid] = nil
+			delete(tran.tran_cache, roleid)
+		} else {
+			// 如果有排队的
+			// 用本尊替换实体
+			rolec.role, _ = hardstore.DecodeRole(rolec.role_store.body, rolec.role_store.rela, rolec.role_store.vers)
+			// 删除占用标记
+			rolec.tran_id = ""
+			// 得到第一个等待的队列
+			wait_first := rolec.wait_line[0]
+			// 构造新的waitline
+			if wait_count == 1 {
+				rolec.wait_line = make([]*tranAskGetRole, 0)
+			} else {
+				new_wait_line := make([]*tranAskGetRole, 0)
+				new_wait_line = rolec.wait_line[1:]
+				rolec.wait_line = new_wait_line
+			}
+			// 修改占用标记
+			rolec.tran_id = wait_first.tran_id
+			// 发送允许的信息
+			wait_first.approved <- true
+			// 给这个角色解锁
+			rolec.lock.Unlock()
+		}
+	}
+	// 删除这个事务
+	delete(t.transaction, signal.tran_id)
 }
 
 func (t *TRule) StoreRole(role roles.Roleer) (err error) {
