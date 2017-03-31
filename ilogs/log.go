dac9208963353a10995157171605bd360e0ab084
Fire Meditation <firemeditation@gmail.com>
2017-03-31 10:56:45 +0800

    just change ilogs

--- ilogs/log.go
+++ ilogs/log.go
@@ -9,144 +9,151 @@
 package ilogs
 
 import (
-	"log"
-	"io"
-	"os"
 	"fmt"
-	
+	"io"
+	"log"
+	"os"
+
 	"github.com/idcsource/Insight-0-0-lib/pubfunc"
 )
 
 // ilogs日志结构
 type Logs struct {
-	runFile				*forsmcslog										//运行日志For SMCS
-	runLog				*log.Logger										//运行日志
-	errFile				*forsmcslog										//错误日志For SMCS
-	errLog				*log.Logger										//错误日志
-	forsmcs				bool											//是否为smcs准备数据
+	runFile *forsmcslog //运行日志For SMCS
+	runLog  *log.Logger //运行日志
+	errFile *forsmcslog //错误日志For SMCS
+	errLog  *log.Logger //错误日志
+	forsmcs bool        //是否为smcs准备数据
+}
+
+func NewLog(run, el, prefix string) (logs *Logs, err error) {
+	return newLog(run, el, prefix, false)
+}
+
+func NewLogForSmcs(prefix string) (logs *Logs, err error) {
+	return newLog("", "", prefix, true)
 }
 
 // 创建新的日志，forsmcs如果为true，则可以使用ReErrLog()和ReRunLog()方法返回在上次调用这两个函数时所产生的日志条目。
-func NewLog(run, el, prefix string, forsmcs bool) (logs *Logs, err error) {
-	var rlogw io.Writer;
+func newLog(run, el, prefix string, forsmcs bool) (logs *Logs, err error) {
+	prefix = prefix + " "
+	var rlogw io.Writer
 	// 如果没有指定文件，则打开一个不存在的东西
 	if len(run) == 0 {
-		rlogw = &nolog{};
-	}else{
-		run = pubfunc.LocalFile(run);
-		rlogw, err = os.OpenFile(run, os.O_WRONLY | os.O_APPEND | os.O_CREATE , 0660);
+		rlogw = &nolog{}
+	} else {
+		run = pubfunc.LocalFile(run)
+		rlogw, err = os.OpenFile(run, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0660)
 		if err != nil {
-			return;
+			return
 		}
 	}
-	var runlogs *log.Logger;
-	smcslogrun := newForsmcslog(rlogw);
+	var runlogs *log.Logger
+	smcslogrun := newForsmcslog(rlogw)
 	if forsmcs == true {
-		runlogs = log.New(smcslogrun, prefix, log.Ldate | log.Ltime);
+		runlogs = log.New(smcslogrun, prefix, log.Ldate|log.Ltime)
 	} else {
-		runlogs = log.New(rlogw, prefix, log.Ldate | log.Ltime);
+		runlogs = log.New(rlogw, prefix, log.Ldate|log.Ltime)
 	}
-	
-	var elogw io.Writer;
+
+	var elogw io.Writer
 	if len(el) == 0 {
-		elogw = &nolog{};
-	}else{
-		el = pubfunc.LocalFile(el);
-		elogw, err = os.OpenFile(el, os.O_WRONLY | os.O_APPEND | os.O_CREATE , 0660);
+		elogw = &nolog{}
+	} else {
+		el = pubfunc.LocalFile(el)
+		elogw, err = os.OpenFile(el, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0660)
 		if err != nil {
-			return;
+			return
 		}
 	}
-	var errlogs *log.Logger;
-	smcslogerr := newForsmcslog(elogw);
+	var errlogs *log.Logger
+	smcslogerr := newForsmcslog(elogw)
 	if forsmcs == true {
-		errlogs = log.New(smcslogerr, prefix, log.Ldate | log.Ltime);
+		errlogs = log.New(smcslogerr, prefix, log.Ldate|log.Ltime)
 	} else {
-		errlogs = log.New(elogw, prefix, log.Ldate | log.Ltime);
+		errlogs = log.New(elogw, prefix, log.Ldate|log.Ltime)
 	}
-	
+
 	if forsmcs == true {
 		logs = &Logs{
-			runFile : smcslogrun,
-			runLog : runlogs,
-			errFile : smcslogerr,
-			errLog : errlogs,
-			forsmcs : true,
-		};
+			runFile: smcslogrun,
+			runLog:  runlogs,
+			errFile: smcslogerr,
+			errLog:  errlogs,
+			forsmcs: true,
+		}
 	} else {
 		logs = &Logs{
-			runLog : runlogs,
-			errLog : errlogs,
-			forsmcs : false,
-		};
+			runLog:  runlogs,
+			errLog:  errlogs,
+			forsmcs: false,
+		}
 	}
-	return;
+	return
 }
 
 // 加入一条运行日志
-func (l *Logs) RunLog (s ...interface{}) {
-		l.runLog.Print(s...);
+func (l *Logs) RunLog(s ...interface{}) {
+	l.runLog.Print(s...)
 }
 
 // 加入一条错误日志
-func (l *Logs) ErrLog (s ...interface{}) {
-		l.errLog.Print(s...);
+func (l *Logs) ErrLog(s ...interface{}) {
+	l.errLog.Print(s...)
 }
 
 // 返回期间的运行日志
-func (l *Logs) ReRunLog () []string {
+func (l *Logs) ReRunLog() []string {
 	if l.forsmcs == true {
-		return l.runFile.ReSMCS();
+		return l.runFile.ReSMCS()
 	} else {
-		return nil;
+		return nil
 	}
 }
 
 // 返回期间的错误日志
-func (l *Logs) ReErrLog () []string {
+func (l *Logs) ReErrLog() []string {
 	if l.forsmcs == true {
-		return l.errFile.ReSMCS();
+		return l.errFile.ReSMCS()
 	} else {
-		return nil;
+		return nil
 	}
 }
-
 
 type nolog struct {
 	n string
 }
 
-func (nl *nolog)Write(p []byte)( n int, err error){
-	n = len(p);
-	err = nil;
-	fmt.Println(string(p));
-	return;
+func (nl *nolog) Write(p []byte) (n int, err error) {
+	n = len(p)
+	err = nil
+	fmt.Println(string(p))
+	return
 }
-
 
 type forsmcslog struct {
-	file		io.Writer
-	smcsd		[]string
+	file  io.Writer
+	smcsd []string
 }
 
-func newForsmcslog (file io.Writer) *forsmcslog {
-	return &forsmcslog{file :file, smcsd : make([]string,0)};
+func newForsmcslog(file io.Writer) *forsmcslog {
+	return &forsmcslog{file: file, smcsd: make([]string, 0)}
 }
 
-func (f *forsmcslog) Write (p []byte) ( n int, err error) {
-	f.smcsd = append(f.smcsd, string(p));
-	fmt.Fprint(f.file, string(p));
-	n = len(p);
-	err = nil;
-	return;
+func (f *forsmcslog) Write(p []byte) (n int, err error) {
+	f.smcsd = append(f.smcsd, string(p))
+	fmt.Fprint(f.file, string(p))
+	n = len(p)
+	err = nil
+	return
 }
 
-func (f *forsmcslog) ReSMCS () []string {
-	ther := make([]string,0);
+func (f *forsmcslog) ReSMCS() []string {
+	ther := make([]string, 0)
 	for _, one := range f.smcsd {
-		ther = append(ther, one);
+		ther = append(ther, one)
 	}
-	
-	f.smcsd = make([]string,0);
-	return ther;
+
+	f.smcsd = make([]string, 0)
+	return ther
 }
