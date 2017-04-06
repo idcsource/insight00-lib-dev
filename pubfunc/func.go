549ab68cd2911e81aff0f633e2c4079709168c36
Fire Meditation <firemeditation@gmail.com>
2017-04-06 10:21:14 +0800

    nothing

--- pubfunc/func.go
+++ pubfunc/func.go
@@ -1,8 +1,8 @@
-// Copyright 2016
+// Copyright 2016-2017
 // CoderG the 2016 project
 // Insight 0+0 [ 洞悉 0+0 ]
 // InDimensions Construct Source [ 忆黛蒙逝·建造源 ]
-// Normal Fire Meditation Qin [ 火志溟 ] -> firemeditation@gmail.com
+// Stephen Fire Meditation Qin [ 火志溟 ] -> firemeditation@gmail.com
 // Use of this source code is governed by GNU LGPL v3 license
 
 // Insight 0+0各包共同使用的辅助函数
