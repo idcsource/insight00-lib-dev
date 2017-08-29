e829101e26428ee18d6759dfa3703d29b3060c28
Fire Meditation <firemeditation@gmail.com>
2017-08-29 11:09:38 +0800

    go one

--- roles/role.go
+++ roles/role.go
@@ -8,10 +8,13 @@
 package roles
 
 import (
+	"bytes"
 	"encoding/gob"
 	"errors"
 	"fmt"
 	"reflect"
+
+	"github.com/idcsource/insight00-lib/iendecode"
 )
 
 const (
@@ -97,6 +100,75 @@ type Status struct {
 	Float   []float64
 	Complex []complex128
 	String  []string
+}
+
+func (s *Status) EncodeBinary() (b []byte, lens int64, err error) {
+	var int_b []byte // 80
+	int_b, err = iendecode.ToBinary(s.Int)
+	if err != nil {
+		return
+	}
+	var float_b []byte // 80
+	float_b, err = iendecode.ToBinary(s.Float)
+	if err != nil {
+		return
+	}
+	var complex_b []byte // 160
+	complex_b, err = iendecode.ToBinary(s.Complex)
+	if err != nil {
+		return
+	}
+	string_buf := bytes.Buffer{}
+	var string_b_len int64 = 0
+	for i := range s.String {
+		sb := []byte(s.String[i])
+		sb_l := int64(len(sb))
+		if sb_l != 0 {
+			string_b_len += 8 + sb_l
+			string_buf.Write(iendecode.Uint64ToBytes(uint64(sb_l)))
+			string_buf.Write(sb)
+		} else {
+			string_b_len += 8
+			string_buf.Write(iendecode.Uint64ToBytes(uint64(sb_l)))
+		}
+	}
+	string_b := string_buf.Bytes()
+	lens = 320 + string_b_len
+	b = make([]byte, lens)
+	copy(b, int_b)
+	copy(b[80:], float_b)
+	copy(b[160:], complex_b)
+	copy(b[320:], string_b)
+	return
+}
+
+func (s *Status) DecodeBinary(b []byte) (err error) {
+	err = iendecode.FromBinary(b[0:80], &s.Int)
+	if err != nil {
+		return
+	}
+	err = iendecode.FromBinary(b[80:160], &s.Float)
+	if err != nil {
+		return
+	}
+	err = iendecode.FromBinary(b[160:320], &s.Complex)
+	if err != nil {
+		return
+	}
+	j := 0
+	var i uint64 = 320
+	for {
+		if i >= uint64(len(b)) {
+			break
+		}
+		slen := iendecode.BytesToUint64(b[i : i+8])
+		if slen != 0 {
+			s.String[j] = string(b[i+8 : i+8+slen])
+		}
+		i += 8 + slen
+		j++
+	}
+	return
 }
 
 // 新建自己
