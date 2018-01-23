f132b740b629b1a4eb5ce34dd15a9465eb5a63d0
Fire Meditation <firemeditation@gmail.com>
2018-01-23 16:21:05 +0800

    p2p

---
+++ p2p/sendfinder.go
@@ -0,0 +1,21 @@
+// Copyright 2016-2018
+// CoderG the 2016 project
+// Insight 0+0 [ 洞悉 0+0 ]
+// InDimensions Construct Source [ 忆黛蒙逝·建造源 ] -> idcsource@gmail.com
+// Stephen Fire Meditation Qin [ 火志溟 ] -> firemeditation@gmail.com
+// This source code is governed by GNU LGPL v3 license
+
+package p2p
+
+import (
+	"github.com/idcsource/insight00-lib/nst2"
+)
+
+// Send and find the data.
+type SendFinder struct {
+	NodesTable *NodesTable // The nodes table.
+}
+
+func (o *SendFinder) NSTexec(ce *nst2.ConnExec) (ss nst2.SendStat, err error) {
+	return
+}
